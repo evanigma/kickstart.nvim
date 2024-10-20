dfc5b915595e3b4bb1b2b08dd92222f7457a1227
Evan <evan@pop-os.foxhome.win>
2024-10-19 22:57:36 -0400

    Add gen.nvim (for ollama LLM integration)

--- init.lua
+++ init.lua
@@ -228,6 +228,15 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+
+  {
+    'David-Kunz/gen.nvim',
+    lazy = true,
+    cmd = 'Gen',
+    config = function()
+      vim.keymap.set({ 'n', 'i' }, '<leader>*', ':Gen<CR>')
+    end,
+  },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
